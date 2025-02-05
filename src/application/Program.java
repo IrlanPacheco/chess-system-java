fd23eac484d0510071b7283e1728233cc693d293
IrlanPacheco <irlan@bol.com.br>
2025-02-05 18:41:48 -0300

    inicio

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+public class Program {
+
+	//C:\Irlan\Udemy\Projetos\chess_system
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!!!");
+
+	}
+
+}
