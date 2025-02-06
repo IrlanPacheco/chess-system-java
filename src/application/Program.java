ef887b389a0b109b91f29012262b71f833212d3c
IrlanPacheco <irlan@bol.com.br>
2025-02-06 10:50:00 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,15 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	//C:\Irlan\Udemy\Projetos\chess_system
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World!!! ");
+		Position pos = new Position(3,5);
+		System.out.println(pos);
 
 	}
 
